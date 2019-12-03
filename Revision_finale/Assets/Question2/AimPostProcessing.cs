3880b6e151b1562407d33feaa1f678a585d0251a
B533P-09\533 <aaaa@aaaa.com>
2019-12-03 10:04:51 -0500

    commit question 2

---
+++ Revision_finale/Assets/Question2/AimPostProcessing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class AimPostProcessing : MonoBehaviour
+{
+
+	  private Material material;
+    public Texture2D textureAim;
+    // Use this for initialization
+
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/ShaderQ2"));
+        material.SetTexture("_AimTexture", textureAim);
+    }
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+}
